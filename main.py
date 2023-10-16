653b05320e5b4a9fd12e1204ac0ea310b1aff79d
Dawid Marczyk <marczyk08@gmail.com>
2023-10-16 19:36:15 +0200

    Wykonane ćwiczenie

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-def cylinder_area(r:float,h:float):
-    """Obliczenie pola powierzchni walca. 
+import math
+import numpy as np
+from numpy import linalg
+def cylinder_area(r: float,h: float) -> float:
+    """Obliczenie pola powierzchni walca.
     Szczegółowy opis w zadaniu 1.
-    
+
     Parameters:
-    r (float): promień podstawy walca 
+    r (float): promień podstawy walca
     h (float): wysokosć walca
-    
+
     Returns:
-    float: pole powierzchni walca 
+    float: pole powierzchni walca
     """
-    return None
+    if r > 0 and h > 0:
+        return 2*math.pi*r**2 + 2*math.pi*r*h
+    return np.NaN
 
 def fib(n:int):
     """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego. 
@@ -21,7 +26,17 @@ def fib(n:int):
     Returns:
     np.ndarray: wektor n pierwszych wyrazów ciągu Fibonnaciego.
     """
-    return None
+    if n <= 0 or type(n) is not int:
+        return None
+    elif n == 1:
+        return np.array([1])
+    elif n == 2:
+        return np.array([1, 1])
+    else:
+        fib_seq = np.array([1, 1])
+        for i in range(2, n):
+            fib_seq = np.append(fib_seq, (fib_seq[i - 1] + fib_seq[i - 2]))
+        return fib_seq.reshape(1, -1)
 
 def matrix_calculations(a:float):
     """Funkcja zwraca wartości obliczeń na macierzy stworzonej 
@@ -35,7 +50,18 @@ def matrix_calculations(a:float):
     touple: krotka zawierająca wyniki obliczeń 
     (Minv, Mt, Mdet) - opis parametrów w zadaniu 4.
     """
-    return None
+    M = np.array([[a, 1, -a], [0, 1, 1], [-a, a, 1]])
+
+    Mdet = linalg.det(M)
+
+    Minv = np.NaN
+    if Mdet != 0:
+        Minv = linalg.inv(M)
+
+    Mt = M.T
+
+    return Minv, Mt, Mdet
+
 
 def custom_matrix(m:int, n:int):
     """Funkcja zwraca macierz o wymiarze mxn zgodnie 
@@ -48,4 +74,13 @@ def custom_matrix(m:int, n:int):
     Returns:
     np.ndarray: macierz zgodna z opisem z zadania 7.
     """
-    return None
+    if m <= 0 or n <= 0 or (type(n) or type(m)) is not int:
+        return None
+    mx = np.zeros((m, n))
+    for i in range(m):
+        for j in range(n):
+            if i > j:
+                mx[i][j] = i
+            else:
+                mx[i][j] = j
+    return mx
